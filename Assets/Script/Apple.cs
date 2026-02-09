dfdc0bac8db33ccc94da4c7a0e413363aaae2660
TheKappaOne <79160101+TheKappaOne@users.noreply.github.com>
2026-02-09 11:13:36 -0500

    applepicker

---
+++ Assets/Script/Apple.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{                                   // a
+    public static float bottomY = -20f;                             // b
+
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+
+             // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();           // b
+             // Call the public AppleMissed() method of apScript
+            apScript.AppleMissed();      // c
+        }
+    }
+}
---
+++ Assets/Script/AppleTree.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using static UnityEditor.PlayerSettings;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]                                                  // a
+                                                                           // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float changeDirChance = 0.1f;
+
+    // Seconds between Apples instantiations
+    public float appleDropDelay = 1f;
+    // Start dropping apples                                          
+    void Start()
+    {
+        // Start dropping apples                                          
+        Invoke("DropApple", 2f);                                        // a
+    }
+
+    void DropApple()
+    {                                                    // b
+        GameObject apple = Instantiate<GameObject>(applePrefab);        // c
+        apple.transform.position = transform.position;                    // d
+        Invoke("DropApple", appleDropDelay);                            // e
+    }
+    void Update()
+    {
+                // Basic Movement
+                Vector3 pos = transform.position;
+                pos.x += speed * Time.deltaTime;
+                transform.position = pos;
+
+                // Changing Direction     
+                if (pos.x < -leftAndRightEdge)
+                {
+                    speed = Mathf.Abs(speed);   // Move right                         
+                }
+                else if (pos.x > leftAndRightEdge)
+                {
+                    speed = -Mathf.Abs(speed);  // Move left                           
+                }
+            }
+
+        void FixedUpdate() {
+            // Random direction changes are now time-based due to FixedUpdate()
+            if (Random.value < changeDirChance)
+            {
+                speed *= -1; // Change direction 
+            }
+        }
+  }
+
+ 
